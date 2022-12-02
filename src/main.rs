685bc0d9d0f4d6708d3e1794d2a9cc6129be694d
Katherine Broadbent <kategory.bee@gmail.com>
2022-12-03 05:50:59 +1000

    solve day 2 part 1

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,37 @@ use std::{
 };
 
 fn main() {
+    let input = File::open("input-day-2.txt").unwrap();
+    let lines = BufReader::new(input).lines();
+
+    let mut score = 0;
+
+    for line in lines {
+        let line = line.unwrap();
+        score += match line.as_str() {
+            // opponent picks rock
+            "A X" => 1 + 3, // i pick rock
+            "A Y" => 2 + 6, // paper
+            "A Z" => 3,     // scissors
+            // picks paper
+            "B X" => 1,
+            "B Y" => 2 + 3,
+            "B Z" => 3 + 6,
+            // picks scissors
+            "C X" => 1 + 6,
+            "C Y" => 2,
+            "C Z" => 3 + 3,
+            unknown => {
+                println!("unexpected entry {}", unknown);
+                0
+            }
+        };
+    }
+
+    println!("total score: {}", score);
+}
+
+fn _day1() {
     let input = File::open("input-day-1.txt").unwrap();
     let lines = BufReader::new(input).lines();
 
