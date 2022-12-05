135fdfe93a640b1dc927a1fdc7ec7e8869e51b8e
Katherine Broadbent <kategory.bee@gmail.com>
2022-12-05 17:10:21 +1000

    day 4 part 1

--- src/main.rs
+++ src/main.rs
@@ -3,33 +3,28 @@ use std::{
     io::{BufRead, BufReader},
 };
 
-fn score_for(ch: u8) -> usize {
-    match ch {
-        b'a'..=b'z' => (ch - b'a' + 1).into(),
-        b'A'..=b'Z' => (ch - b'A' + 27).into(),
-        // 'A'u8 .. 'Z'u8 => 0,
-        _ => panic!("unexpected char"),
-    }
-}
-
 fn main() {
-    let input = File::open("input-day-3.txt").unwrap();
+    let input = File::open("input-day-4.txt").unwrap();
     let lines: std::io::Result<Vec<String>> = BufReader::new(input).lines().collect();
 
     let mut score = 0;
 
-    let chars = (b'a'..=b'z').chain(b'A'..=b'Z');
+    for line in lines.unwrap() {
+        let vals: Vec<&str> = line.split(&['-', ',']).collect();
 
-    for line in lines.unwrap().as_slice().chunks_exact(3) {
-        let str_1 = line[0].as_bytes();
-        let str_2 = line[1].as_bytes();
-        let str_3 = line[2].as_bytes();
+        let x_low: i32 = vals[0].parse().unwrap();
+        let x_high: i32 = vals[1].parse().unwrap();
+        let y_low: i32 = vals[2].parse().unwrap();
+        let y_high: i32 = vals[3].parse().unwrap();
 
-        // we're guaranteed exactly one common item type, no more, no less.
-        for ch in chars.clone() {
-            if str_1.contains(&ch) && str_2.contains(&ch) && str_3.contains(&ch) {
-                score += score_for(ch);
-            }
+        if x_low >= y_low && x_high <= y_high {
+            // x contained within y
+            score += 1;
+        } else if y_low >= x_low && y_high <= x_high {
+            // y contained within x
+            score += 1;
+        } else {
+            println!("{}", line);
         }
     }
 
