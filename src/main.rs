1ff6af235fcc731090fba511319fbaef57c465b4
Katherine Broadbent <kategory.bee@gmail.com>
2022-12-05 17:31:27 +1000

    day 5 part 1
    could have printed it nicer at the end, but eh it works

--- src/main.rs
+++ src/main.rs
@@ -4,28 +4,46 @@ use std::{
 };
 
 fn main() {
-    let input = File::open("input-day-4.txt").unwrap();
+    // i've decided to move the initial container configuration in to code, seems easier than
+    // parsing.
+    // [V]     [B]                     [C]
+    // [C]     [N] [G]         [W]     [P]
+    // [W]     [C] [Q] [S]     [C]     [M]
+    // [L]     [W] [B] [Z]     [F] [S] [V]
+    // [R]     [G] [H] [F] [P] [V] [M] [T]
+    // [M] [L] [R] [D] [L] [N] [P] [D] [W]
+    // [F] [Q] [S] [C] [G] [G] [Z] [P] [N]
+    // [Q] [D] [P] [L] [V] [D] [D] [C] [Z]
+    //  1   2   3   4   5   6   7   8   9
+    // we'll use base 0, and remember to fix when we read the instructions
+    let mut stacks = vec![
+        vec!['Q', 'F', 'M', 'R', 'L', 'W', 'C', 'V'],
+        vec!['D', 'Q', 'L'],
+        vec!['P', 'S', 'R', 'G', 'W', 'C', 'N', 'B'],
+        vec!['L', 'C', 'D', 'H', 'B', 'Q', 'G'],
+        vec!['V', 'G', 'L', 'F', 'Z', 'S'],
+        vec!['D', 'G', 'N', 'P'],
+        vec!['D', 'Z', 'P', 'V', 'F', 'C', 'W'],
+        vec!['C', 'P', 'D', 'M', 'S'],
+        vec!['Z', 'N', 'W', 'T', 'V', 'M', 'P', 'C'],
+    ];
+
+    let input = File::open("input-day-5.txt").unwrap();
     let lines: std::io::Result<Vec<String>> = BufReader::new(input).lines().collect();
 
-    let mut score = 0;
-
     for line in lines.unwrap() {
-        let vals: Vec<&str> = line.split(&['-', ',']).collect();
+        // each line is 'move <count> from <src> to <dst>', like 'move 1 from 9 to 2'
+        let vals: Vec<&str> = line.split(' ').collect();
 
-        let x_low: i32 = vals[0].parse().unwrap();
-        let x_high: i32 = vals[1].parse().unwrap();
-        let y_low: i32 = vals[2].parse().unwrap();
-        let y_high: i32 = vals[3].parse().unwrap();
+        let count: i32 = vals[1].parse().unwrap();
+        let source: usize = vals[3].parse::<usize>().unwrap() - 1;
+        let dest: usize = vals[5].parse::<usize>().unwrap() - 1;
 
-        let x_range = x_low..=x_high;
-        let y_range = y_low..=y_high;
-
-        if x_range.contains(&y_low) || x_range.contains(&y_high) {
-            score += 1;
-        } else if y_range.contains(&x_low) || y_range.contains(&x_high) {
-            score += 1;
+        for _ in 0..count {
+            let cr = stacks[source].pop().unwrap();
+            stacks[dest].push(cr);
         }
     }
 
-    println!("score {}", score);
+    dbg!(stacks);
 }
