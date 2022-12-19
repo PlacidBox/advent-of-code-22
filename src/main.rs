b18a3181ec884587cc9f36c457d01448824c638d
Katherine Broadbent <kategory.bee@gmail.com>
2022-12-19 18:38:29 +1000

    day 15 part 1
    an exercise in hubris. had multiple hours spend tracking
    down bugs, and it was that the number of beacons in
    the input range is 1, not 3. I counted 3 because of the
    number of input rows covering that single beacon.
    
    fucks sake

--- src/main.rs
+++ src/main.rs
@@ -1,110 +1,136 @@
-use std::{
-    collections::HashSet,
-    fs::File,
-    io::{BufRead, BufReader},
-};
+// easier than parsing. list of (sensor, beacon), where each is an (x, y) pair.
+const INPUT: &[((i32, i32), (i32, i32))] = &[
+    ((3523437, 2746095), (3546605, 2721324)),
+    ((282831, 991087), (743030, -87472)),
+    ((1473740, 3283213), (1846785, 3045894)),
+    ((1290563, 46916), (743030, -87472)),
+    ((3999451, 15688), (3283637, -753607)),
+    ((1139483, 2716286), (1846785, 3045894)),
+    ((3137614, 2929987), (3392051, 3245262)),
+    ((2667083, 2286333), (2126582, 2282363)),
+    ((3699264, 2920959), (3546605, 2721324)),
+    ((3280991, 2338486), (3546605, 2721324)),
+    ((833202, 92320), (743030, -87472)),
+    ((3961416, 2485266), (3546605, 2721324)),
+    ((3002132, 3500345), (3392051, 3245262)),
+    ((2482128, 2934657), (1846785, 3045894)),
+    ((111006, 2376713), (354526, 3163958)),
+    ((424237, 2718408), (354526, 3163958)),
+    ((3954504, 3606495), (3392051, 3245262)),
+    ((2275050, 2067292), (2333853, 2000000)),
+    ((1944813, 2557878), (2126582, 2282363)),
+    ((2227536, 2152792), (2126582, 2282363)),
+    ((3633714, 1229193), (3546605, 2721324)),
+    ((1446898, 1674290), (2333853, 2000000)),
+    ((3713985, 2744503), (3546605, 2721324)),
+    ((2281504, 3945638), (1846785, 3045894)),
+    ((822012, 3898848), (354526, 3163958)),
+    ((89817, 3512049), (354526, 3163958)),
+    ((2594265, 638715), (2333853, 2000000)),
+];
 
-const SPAWN: (i32, i32) = (500, 0);
+const TARGET_ROW: i32 = 2000000;
+const BEACONS_IN_TARGET_ROW: i32 = 1;
 
-// set of all positions that are filled. x, y. icreasing y is 'down'
-fn load() -> (HashSet<(i32, i32)>, i32) {
-    let mut res = HashSet::new();
-    let mut max_y = 0;
-
-    let input = File::open("input.txt").unwrap();
-    let lines = BufReader::new(input).lines();
-
-    for line in lines {
-        let line = line.unwrap();
-        let mut pen_points = line.split(" -> ").map(parse_point);
-
-        let mut start_point = pen_points.next().unwrap();
-        max_y = max_y.max(start_point.1);
-
-        for end_point in pen_points {
-            max_y = max_y.max(end_point.1);
-            plot_line(start_point, end_point, &mut res);
-            start_point = end_point;
-        }
-    }
-
-    (res, max_y)
-}
-
-fn parse_point(input: &str) -> (i32, i32) {
-    let mut digits = input.split(',');
-    let x = digits.next().unwrap();
-    let y = digits.next().unwrap();
-    assert!(digits.next() == None);
-
-    (x.parse().unwrap(), y.parse().unwrap())
-}
-
-fn plot_line(start_point: (i32, i32), end_point: (i32, i32), res: &mut HashSet<(i32, i32)>) {
-    let dx = if start_point.0 < end_point.0 {
-        1
-    } else if start_point.0 > end_point.0 {
-        -1
-    } else {
-        0
-    };
-
-    let dy = if start_point.1 < end_point.1 {
-        1
-    } else if start_point.1 > end_point.1 {
-        -1
-    } else {
-        0
-    };
-
-    let mut current = start_point;
-    while current != end_point {
-        res.insert(current);
-        current.0 += dx;
-        current.1 += dy;
-    }
-
-    res.insert(end_point);
+// Forms a half-open range. so when is_entry is _true_, the cell (x_coord, ROW) is _not_ a beacon,
+// if it's _false_, it could be a beacon.
+//
+// The edge is between `x_coord - 1` and `x_coord`
+#[derive(Debug, Clone, Copy)]
+struct SensorEdge {
+    x_coord: i32,
+    is_entry: bool,
 }
 
 fn main() {
-    let (mut field, max_y) = load();
+    // convert to list of 'distances' from each sensor that can't be beacons (or is a beacon). this
+    // gives us the entry and exit cell for each sensor along the row. track entering an area as +1,
+    // exiting as -1, any time we're positive there can't be a non-detected-beacon.
+    //
+    // so sort the list of edges, traverse counting how many sensors are covered, and sum up
+    // positive patches
+    //
+    // then remove 3 because there's 3 beacons detected in that row, and the answer doesn't include
+    // those.
+    let mut edges = vec![];
 
-    let mut settled_sand = 0;
-    let mut pos = SPAWN;
+    for ((s_x, s_y), (b_x, b_y)) in INPUT {
+        // get distance around sensor (manhatten distance)
+        let dx = s_x.abs_diff(*b_x);
+        let dy = s_y.abs_diff(*b_y);
+        let distance = dx + dy;
 
-    while !field.contains(&SPAWN) {
-        let below_l = field.contains(&(pos.0 - 1, pos.1 + 1));
-        let below_m = field.contains(&(pos.0, pos.1 + 1));
-        let below_r = field.contains(&(pos.0 + 1, pos.1 + 1));
+        // range left/right along the target row:
+        // for a sensor of distance 2:
+        // ..#..
+        // .B##.
+        // ##S##
+        // .###. <-- target row
+        // ..#..
+        // distance taken by vertical: 1, so 1 remaining either side at (s_x, TARGET_ROW)
+        let range_to_tgt_row = s_y.abs_diff(TARGET_ROW);
 
-        match (below_l, below_m, below_r) {
-            (true, true, true) => {
-                // comes to standstill, sand solidifies to block future sand
-                settled_sand += 1;
-                field.insert(pos);
-                pos = SPAWN;
-            }
-            (_, false, _) => pos.1 += 1, // falls directly downwards
-            (false, true, _) => {
-                // prefers to move to the left, if below is blocked
-                pos.0 -= 1;
-                pos.1 += 1;
-            }
-            (true, true, false) => {
-                // otherwise falls to the right
-                pos.0 += 1;
-                pos.1 += 1;
-            }
-        }
+        if range_to_tgt_row <= distance {
+            let range = distance - range_to_tgt_row;
+            let left_edge = SensorEdge {
+                x_coord: s_x - range as i32,
+                is_entry: true,
+            };
 
-        if pos.1 == max_y + 1 {
-            // resting above the infinite ground plane
-            settled_sand += 1;
-            field.insert(pos);
-            pos = SPAWN;
+            let right_edge = SensorEdge {
+                x_coord: s_x + range as i32 + 1,
+                is_entry: false,
+            };
+
+            // 4985188
+            // too low
+
+            // 4985191
+
+            println!(
+                "Sensor ({},{}), B ({},{}) covers target row from {} to {}",
+                s_x, s_y, b_x, b_y, left_edge.x_coord, right_edge.x_coord
+            );
+
+            edges.push(left_edge);
+            edges.push(right_edge);
         }
     }
 
-    println!("{}", settled_sand);
+    edges.sort_by_key(|edge| edge.x_coord);
+
+    let mut covered_cells = 0;
+    let mut entered_at = None;
+    let mut coverage_count = 0;
+
+    for edge in &edges {
+        if edge.is_entry && coverage_count == 0 {
+            assert_eq!(entered_at, None);
+            entered_at = Some(edge.x_coord);
+            println!("Coverage starts at x={}", edge.x_coord);
+        }
+
+        if !edge.is_entry && coverage_count == 1 {
+            //           1    1
+            // 0    5    0    5
+            // ....######......
+            //     ^     ^
+            // entry=4    exit=10
+            // distance covered = 6
+            let distance_covered = edge.x_coord - entered_at.unwrap();
+            covered_cells += distance_covered;
+            entered_at = None;
+            println!("Coverage starts at x={}", edge.x_coord);
+        }
+
+        match edge.is_entry {
+            true => coverage_count += 1,
+            false => coverage_count -= 1,
+        }
+    }
+
+    assert_eq!(entered_at, None);
+    assert_eq!(coverage_count, 0);
+
+    println!("answer: {}", covered_cells - BEACONS_IN_TARGET_ROW);
 }
